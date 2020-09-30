6263f6396ca1605a459a53890200ad50d7b5ebd3
Willard19 <60764938+Willard19@users.noreply.github.com>
2020-09-30 02:37:43 -0600

    Segundo Commit

--- bcksecom/routes/index.js
+++ bcksecom/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/info',function(req,res)
+{
+  var infoObj={}; //infoObj=array(); en php
+  infoObj.cuenta="0801199807811";
+  infoObj.nombre="Willard Javier Raudales Alvarenga";
+  infoObj.email="willard@unicah.edu";
+  infoObj.telefono="32144094";
+  res.json(infoObj);
+}
+
+);//get info
 module.exports = router;
+
